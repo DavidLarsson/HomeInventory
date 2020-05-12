97b9c0461742d89abcf559dda28a88512fb8048b
David Larsson <2537886+DavidLarsson@users.noreply.github.com>
2020-05-12 15:16:43 +0200

    Modified to make use of the Python3 virtual environment and pip. Also added automatic upgrade of pip to fix some error messages.

--- shell.nix
+++ shell.nix
@@ -1,12 +1,18 @@
 with import <nixpkgs> {};
 
 let
-  pythonEnv = python38.withPackages (ps: [
-            ps.numpy
-            ps.toolz
-            ps.django]);
+  pythonEnv = python38;
 in mkShell {
   buildInputs = [
     pythonEnv
     ];
+  shellHook = ''
+    if [ -d ./.venv ]; then
+      rm -rf ./.venv
+    fi
+    python -m venv .venv
+    source .venv/bin/activate
+    pip install --upgrade pip
+    pip install -r requirements.txt;
+  '';
 }
