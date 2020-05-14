675ff3826e7436894351180b4412f4bf0ddfadd6
David Larsson <2537886+DavidLarsson@users.noreply.github.com>
2020-05-14 06:57:12 +0200

    Miscellaneous
    * Started playing with shelve as persistence model
    * Added shelve file storage to .gitignore
    * Added module categories, might be wrong idea
    * Added pytest to requirements

--- inventory_web/inventory_web.py
+++ inventory_web/inventory_web.py
@@ -1,24 +1,35 @@
 from flask import Flask, render_template, url_for, request, redirect
+import shelve
 
 app = Flask(__name__)
+d = shelve.open("storage")
 
-@app.route('/')
+
+@app.route("/")
 def default_route():
     return "Default"
 
-@app.route('/<string:category>/')
+
+@app.route("/<string:category>/")
 def category_filter(category):
     return category
 
-@app.route('/add/')
+
+@app.route("/add/")
 def add_item():
     return "Add item"
 
-@app.route('/shopping/')
-@app.route('/shopping/<string:category>/')
-def shopping_list(category=None):
+
+@app.route("/shopping/")
+@app.route("/shopping/<string:category>/")
+@app.route("/shopping/<string:category>/<string:itemtype>/")
+def shopping_list(category=None, itemtype=None):
     if category == None:
         return "My shopping list"
-    else:
+    elif itemtype == None:
         return category
+    elif itemtype in items(category):
+        return itemtype
 
+
+d.close()
